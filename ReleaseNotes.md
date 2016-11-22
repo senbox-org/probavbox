f0cad3e3e838538eea4e70890c9ee117c7cb0b52
marpet <marco.peters@brockmann-consult.de>
2016-11-22 14:23:47 +0100

    Initial version of Proba-V Toolbox

---
+++ ReleaseNotes.md
@@ -0,0 +1,9 @@
+PROBA-V Toolbox Release Notes
+================================
+
+Changes in PROBA-V Toolbox 1.0
+------------------------------
+
+###New Features and Important Changes
+* Introduced the PROBA-V Toolbox
+* Contains the PROBA-V reader as heritage from the Sentinel-3 Toolbox
---
+++ probavbox-kit/src/main/java/org/vito/probavbox/about/ProbavAboutBox.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.vito.probavbox.about;
+
+import org.esa.snap.rcp.about.AboutBox;
+import org.esa.snap.rcp.util.BrowserUtils;
+import org.openide.modules.ModuleInfo;
+import org.openide.modules.Modules;
+
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import java.awt.Cursor;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author Marco Peters
+ */
+@AboutBox(displayName = "Proba-V", position = 110)
+public class ProbavAboutBox extends JPanel {
+
+    private final static String releaseNotesHTTP = "https://github.com/senbox-org/probav/blob/master/ReleaseNotes.md";
+
+    public ProbavAboutBox() {
+        super(new BorderLayout(4, 4));
+        setBorder(new EmptyBorder(4, 4, 4, 4));
+        ImageIcon aboutImage = new ImageIcon(ProbavAboutBox.class.getResource("about_probav.jpg"));
+        JLabel iconLabel = new JLabel(aboutImage);
+        add(iconLabel, BorderLayout.CENTER);
+        add(createVersionPanel(), BorderLayout.SOUTH);
+    }
+
+    private JPanel createVersionPanel() {
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
+        final ModuleInfo moduleInfo = Modules.getDefault().ownerOf(ProbavAboutBox.class);
+        panel.add(new JLabel("<html><b>Proba-V Toolbox version " + moduleInfo.getImplementationVersion() + "</b>",
+                SwingConstants.RIGHT));
+        final URI releaseNotesURI = getReleaseNotesURI();
+        if (releaseNotesURI != null) {
+            final JLabel releaseNoteLabel = new JLabel("<html><a href=\"" + releaseNotesURI.toString() + "\">Release Notes</a>",
+                                                       SwingConstants.RIGHT);
+            releaseNoteLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            releaseNoteLabel.addMouseListener(new BrowserUtils.URLClickAdaptor(releaseNotesHTTP));
+            panel.add(releaseNoteLabel);
+        }
+
+        return panel;
+    }
+
+    private URI getReleaseNotesURI() {
+        try {
+            return new URI(releaseNotesHTTP);
+        } catch (URISyntaxException e) {
+            return null;
+        }
+    }
+
+
+}
